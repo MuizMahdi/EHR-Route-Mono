f6b4b4982abedaf112bc26b877936921f37de611
MuizMahdi <Moezmahdy@gmail.com>
2018-10-21 00:41:11 +0300

    Initial Commit

---
+++ src/main/java/com/project/EMRChain/EmrChainApplication.java
@@ -0,0 +1,13 @@
+package com.project.EMRChain;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmrChainApplication
+{
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmrChainApplication.class, args);
+	}
+}
