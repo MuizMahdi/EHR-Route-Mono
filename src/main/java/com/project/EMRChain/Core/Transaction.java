259da12de536b7025df89f0e6ca5b5088c49530a
MuizMahdi <Moezmahdy@gmail.com>
2018-10-23 22:40:07 +0300

    Update: Added Key-to-String and String-to-Key conversion on StringUtil

--- src/main/java/com/project/EMRChain/Core/Transaction.java
+++ src/main/java/com/project/EMRChain/Core/Transaction.java
@@ -1,5 +1,6 @@
 package com.project.EMRChain.Core;
 import com.project.EMRChain.Core.Utilities.HashUtil;
+import com.project.EMRChain.Core.Utilities.StringUtil;
 import com.project.EMRChain.EHR.MedicalRecord;
 import com.project.EMRChain.EHR.PatientInfo;
 import com.project.EMRChain.Utilities.JsonUtil;
@@ -11,36 +12,35 @@ import java.util.HashMap;
 public class Transaction
 {
     private String transactionId; // Hash of transaction
-    private MedicalRecord data;
+    private MedicalRecord transactionData;
 
     private PublicKey sender; // Sender's address/public key.
     private PublicKey recipient; // Recipient's address/public key.
     public byte[] signature;
     private static int sequence = 0; // A count of the number of transactions
 
-    private ArrayList<TransactionInput> inputs = new ArrayList<>();
-    private ArrayList<TransactionOutput> outputs = new ArrayList<>();
+    //private ArrayList<TransactionInput> inputs = new ArrayList<>();
+    //private ArrayList<TransactionOutput> outputs = new ArrayList<>();
 
     public Transaction() { }
-    public Transaction(MedicalRecord data, PublicKey sender, PublicKey recipient, ArrayList<TransactionInput> inputs) {
-        this.data = data;
+    public Transaction(MedicalRecord transactionData, PublicKey sender, PublicKey recipient) {
+        this.transactionData = transactionData;
         this.sender = sender;
         this.recipient = recipient;
-        this.inputs = inputs;
+        //this.inputs = inputs;
     }
 
     private byte[] getHash() {
         sequence++; // Increase sequence to avoid 2 transactions having same hash
 
-        if (data == null) {
-           data = new MedicalRecord();
+        if (transactionData == null) {
+            transactionData = new MedicalRecord();
         }
 
-        String data = JsonUtil.createJson(data);
-        String keys = ""; // Todo: convert keys into strings (stringUtil) and add them.
+        String dataString = JsonUtil.createJson(transactionData);
+        String keys = StringUtil.getStringFromKey(sender) + StringUtil.getStringFromKey(recipient);
 
-        byte[] hashedData = (data + keys + sequence).getBytes();
-
+        byte[] hashedData = (dataString + keys + sequence).getBytes();
         return HashUtil.SHA256(hashedData);
     }
 }
--- src/main/java/com/project/EMRChain/Core/Utilities/StringUtil.java
+++ src/main/java/com/project/EMRChain/Core/Utilities/StringUtil.java
@@ -1,7 +1,20 @@
 package com.project.EMRChain.Core.Utilities;
+import org.bouncycastle.jce.provider.BouncyCastleProvider;
+import java.security.*;
+import java.security.spec.X509EncodedKeySpec;
+import java.util.Base64;
 
-/**
- * Created by Muiz Mahdi on 10/21/2018.
- */
-public class StringUtil {
+public class StringUtil
+{
+    public static String getStringFromKey(Key key) {
+        return Base64.getEncoder().encodeToString(key.getEncoded());
+    }
+
+    public static Key getPublicKeyFromString(String stringKey) throws GeneralSecurityException {
+        Security.addProvider(new BouncyCastleProvider()); // Set BC as provider
+        byte[] keyBytes = Base64.getDecoder().decode(stringKey);
+        KeyFactory keyFactory = KeyFactory.getInstance("ECDSA", "BC"); // ECDSA KeyFactory using BC
+        PublicKey publicKey = keyFactory.generatePublic(new X509EncodedKeySpec(keyBytes));
+        return publicKey;
+    }
 }
