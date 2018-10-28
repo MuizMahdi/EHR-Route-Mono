094abecce79b9c739a83cfe1a824842692932957
MuizMahdi <Moezmahdy@gmail.com>
2018-10-28 19:22:32 +0300

    Update: Block TimeStamp added on block creation

--- src/main/java/com/project/EMRChain/Core/BlockHeader.java
+++ src/main/java/com/project/EMRChain/Core/BlockHeader.java
@@ -1,5 +1,7 @@
 package com.project.EMRChain.Core;
 
+import java.util.Date;
+
 public class BlockHeader
 {
     private Hash hash; // Hash of the block
@@ -11,16 +13,19 @@ public class BlockHeader
     // the whole Blockchain tree to validate the whole chain.
     private byte[] merkleRoot;
 
-    public BlockHeader() { }
+    public BlockHeader() {
+        this.timeStamp = new Date().getTime();
+    }
     public BlockHeader(Hash hash, Hash previousHash, byte[] merkleRoot) {
         this.hash = hash;
         this.previousHash = previousHash;
+        this.timeStamp = new Date().getTime();
         this.merkleRoot = merkleRoot;
     }
-    public BlockHeader(Hash hash, Hash previousHash, long timeStamp, long index, byte[] merkleRoot) {
+    public BlockHeader(Hash hash, Hash previousHash, long index, byte[] merkleRoot) {
         this.hash = hash;
         this.previousHash = previousHash;
-        this.timeStamp = timeStamp;
+        this.timeStamp = new Date().getTime();
         this.index = index;
         this.merkleRoot = merkleRoot;
     }
