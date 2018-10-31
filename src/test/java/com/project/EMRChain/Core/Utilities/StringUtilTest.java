3e091a10430c83aa904d268be87fb65e9cfd0f41
MuizMahdi <Moezmahdy@gmail.com>
2018-10-31 22:26:29 +0300

    Test: added StringUtil Test

---
+++ src/test/java/com/project/EMRChain/Core/Utilities/StringUtilTest.java
@@ -0,0 +1,40 @@
+package com.project.EMRChain.Core.Utilities;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.junit.MockitoJUnitRunner;
+import java.security.KeyPair;
+import java.security.PublicKey;
+import static org.junit.Assert.*;
+
+@RunWith(MockitoJUnitRunner.class)
+public class StringUtilTest
+{
+    private final StringUtil stringUtil = new StringUtil();
+    private final byte[] bytes = "String".getBytes();
+
+    @Test
+    public void getStringFromKey() throws Exception
+    {
+        EcdsaUtil ecdsaUtil = new EcdsaUtil();
+        KeyPair keyPair = ecdsaUtil.ecGenerateKeyPair();
+        PublicKey publicKey = keyPair.getPublic();
+
+        assertNotNull(stringUtil.getStringFromKey(publicKey));
+        assertTrue(stringUtil.getStringFromKey(publicKey) instanceof String);
+    }
+
+    @Test
+    public void getStringFromBytes() throws Exception
+    {
+        assertNotNull(stringUtil.getStringFromBytes(bytes));
+        assertTrue(stringUtil.getStringFromBytes(bytes) instanceof String);
+    }
+
+    @Test
+    public void getUTF8StringFromBytes() throws Exception
+    {
+        assertNotNull(stringUtil.getUTF8StringFromBytes(bytes));
+        assertTrue(stringUtil.getUTF8StringFromBytes(bytes) instanceof String);
+        assertEquals(stringUtil.getUTF8StringFromBytes(bytes), "String");
+    }
+}
