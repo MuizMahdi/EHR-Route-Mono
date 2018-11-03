5002be81153e7e1306a3f91dd1d0124f5dfbff5f
MuizMahdi <Moezmahdy@gmail.com>
2018-11-03 20:58:02 +0300

    Update: added Json Web Token Authentication EntryPoint

---
+++ src/main/java/com/project/EMRChain/Security/JwtAuthenticationEntryPoint.java
@@ -0,0 +1,35 @@
+package com.project.EMRChain.Security;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.AuthenticationEntryPoint;
+import org.springframework.stereotype.Component;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+
+/*
+*   This class is used to return a '401 unauthorized error' to clients that try to access a protected
+*   resource without proper authentication. It implements Spring Security’s AuthenticationEntryPoint
+*   interface.
+*
+*   The commence() method is called whenever an exception is thrown due to an unauthenticated user
+*   trying to access a resource that requires authentication.
+*/
+
+
+@Component
+public class JwtAuthenticationEntryPoint implements AuthenticationEntryPoint
+{
+    private static final Logger logger = LoggerFactory.getLogger(JwtAuthenticationEntryPoint.class);
+
+    @Override
+    public void commence(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, AuthenticationException Ex) throws IOException, ServletException
+    {
+        logger.error("Responding with unauthorized error. Message - {}", Ex.getMessage());
+        httpServletResponse.sendError(HttpServletResponse.SC_UNAUTHORIZED, Ex.getMessage());
+    }
+}
