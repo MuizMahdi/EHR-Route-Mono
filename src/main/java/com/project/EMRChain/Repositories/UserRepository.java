b15ad80caeb1ca86a00945fb0b62320b670461ec
MuizMahdi <Moezmahdy@gmail.com>
2018-11-03 15:16:26 +0300

    Update: added User Repository

---
+++ src/main/java/com/project/EMRChain/Repositories/UserRepository.java
@@ -0,0 +1,20 @@
+package com.project.EMRChain.Repositories;
+import com.project.EMRChain.Entities.Auth.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long>
+{
+    Boolean existsByUsername(String username);
+    Boolean existsByEmail(String email);
+
+    Optional<User> findByUsername(String username);
+    Optional<User> findByEmail(String email);
+    Optional<User> findByUsernameOrEmail(String username, String email);
+
+    List<User> findByIdIn(List<Long> userIds);
+}
