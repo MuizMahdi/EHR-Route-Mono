223d23a2b9054f8a54cbb9ed858ed3d25161c831
MuizMahdi <Moezmahdy@gmail.com>
2018-11-04 17:22:07 +0300

    [UPDATE] Added login request payload

---
+++ src/main/java/com/project/EMRChain/Payload/LoginRequest.java
@@ -0,0 +1,24 @@
+package com.project.EMRChain.Payload;
+import javax.validation.constraints.NotBlank;
+
+public class LoginRequest
+{
+    @NotBlank
+    private String usernameOrEmail;
+
+    @NotBlank
+    private String password;
+
+    public String getPassword() {
+        return password;
+    }
+    public String getUsernameOrEmail() {
+        return usernameOrEmail;
+    }
+    public void setPassword(String password) {
+        this.password = password;
+    }
+    public void setUsernameOrEmail(String usernameOrEmail) {
+        this.usernameOrEmail = usernameOrEmail;
+    }
+}
