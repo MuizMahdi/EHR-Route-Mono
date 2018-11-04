3e6750c619a470a3b74aee96443fce85c91b1c2d
MuizMahdi <Moezmahdy@gmail.com>
2018-11-04 17:23:55 +0300

    [UPDATE] Added JsonWebToken authentication response payload

---
+++ src/main/java/com/project/EMRChain/Payload/JwtAuthenticationResponse.java
@@ -0,0 +1,25 @@
+package com.project.EMRChain.Payload;
+
+
+public class JwtAuthenticationResponse
+{
+    private String accessToken;
+    private String tokenType = "Bearer";
+
+    public JwtAuthenticationResponse(String accessToken) {
+        this.accessToken = accessToken;
+    }
+
+    public String getTokenType() {
+        return tokenType;
+    }
+    public String getAccessToken() {
+        return accessToken;
+    }
+    public void setTokenType(String tokenType) {
+        this.tokenType = tokenType;
+    }
+    public void setAccessToken(String accessToken) {
+        this.accessToken = accessToken;
+    }
+}
