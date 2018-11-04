6f5df1cf4fc2baeb6f76cfac87785a7e0a2a1471
MuizMahdi <Moezmahdy@gmail.com>
2018-11-04 17:18:58 +0300

    [UPDATE] Added signup request payload

---
+++ src/main/java/com/project/EMRChain/Payload/SignupRequest.java
@@ -0,0 +1,50 @@
+package com.project.EMRChain.Payload;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+
+public class SignupRequest
+{
+    @NotBlank
+    @Size(min=4, max=40)
+    private String name;
+
+    @NotBlank
+    @Size(min=3, max=15)
+    private String username;
+
+    @NotBlank
+    @Size(max=40)
+    @Email
+    private String email;
+
+    @NotBlank
+    @Size(min=6, max=20)
+    private String password;
+
+    public String getName() {
+        return name;
+    }
+    public String getEmail() {
+        return email;
+    }
+    public String getUsername() {
+        return username;
+    }
+    public String getPassword() {
+        return password;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+    public void setEmail(String email) {
+        this.email = email;
+    }
+    public void setUsername(String username) {
+        this.username = username;
+    }
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
