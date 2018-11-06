096ff59a83c05ec9f709ffb813589be15cb9c02f
MuizMahdi <Moezmahdy@gmail.com>
2018-11-06 20:33:33 +0300

    [Update] Added VerificationToken Repository

---
+++ src/main/java/com/project/EMRChain/Repositories/VerificationTokenRepository.java
@@ -0,0 +1,13 @@
+package com.project.EMRChain.Repositories;
+import com.project.EMRChain.Entities.Auth.VerificationToken;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+
+@Repository
+public interface VerificationTokenRepository extends JpaRepository<VerificationToken, Long>
+{
+    VerificationToken findByToken(String token);
+}
