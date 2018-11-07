e5c9f1ff99df5cbad405b5ad9a24890b3b162d93
MuizMahdi <Moezmahdy@gmail.com>
2018-11-07 23:15:23 +0300

    [Edit] Null check in getCurrentUser

--- src/main/java/com/project/EMRChain/Controllers/UserController.java
+++ src/main/java/com/project/EMRChain/Controllers/UserController.java
@@ -1,10 +1,13 @@
 package com.project.EMRChain.Controllers;
 import com.project.EMRChain.Entities.Auth.User;
+import com.project.EMRChain.Payload.Auth.ApiResponse;
 import com.project.EMRChain.Payload.UserInfo;
 import com.project.EMRChain.Security.CurrentUser;
 import com.project.EMRChain.Security.UserPrincipal;
 import com.project.EMRChain.Services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -25,10 +28,26 @@ public class UserController
 
     @GetMapping("/me")
     @PreAuthorize("hasRole('USER')")
-    public UserInfo getCurrentUser(@CurrentUser UserPrincipal currentUser)
+    public ResponseEntity<?> getCurrentUser(@CurrentUser UserPrincipal currentUser)
     {
         User user = userService.findUserByUsernameOrEmail(currentUser.getUsername());
-        return new UserInfo(user.getId(), user.getUsername(), user.getName(), user.isFirstLogin());
+
+        if (user == null)
+        {
+            return new ResponseEntity<>(
+                new ApiResponse(false, "Invalid username"),
+                HttpStatus.BAD_REQUEST
+            );
+        }
+
+        return ResponseEntity.ok (
+            new UserInfo (
+                user.getId(),
+                        user.getUsername(),
+                        user.getName(),
+                        user.isFirstLogin()
+                )
+        );
     }
 
 }
