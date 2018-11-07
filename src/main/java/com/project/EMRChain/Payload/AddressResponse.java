9d2436ca8ddfc218896f07ba2957eb28f5262a35
MuizMahdi <Moezmahdy@gmail.com>
2018-11-07 23:18:33 +0300

    [Update] Added AddressResponse payload

---
+++ src/main/java/com/project/EMRChain/Payload/AddressResponse.java
@@ -0,0 +1,33 @@
+package com.project.EMRChain.Payload;
+
+public class AddressResponse
+{
+    private String address;
+    private String publicKey;
+    private String privateKey;
+
+    public AddressResponse(String address, String publicKey, String privateKey) {
+        this.address = address;
+        this.publicKey = publicKey;
+        this.privateKey = privateKey;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+    public String getPublicKey() {
+        return publicKey;
+    }
+    public String getPrivateKey() {
+        return privateKey;
+    }
+    public void setAddress(String address) {
+        this.address = address;
+    }
+    public void setPublicKey(String publicKey) {
+        this.publicKey = publicKey;
+    }
+    public void setPrivateKey(String privateKey) {
+        this.privateKey = privateKey;
+    }
+}
