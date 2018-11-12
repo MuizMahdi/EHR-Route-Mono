acf228acf0f4804b178896eb44905a7e6b4e3b8c
MuizMahdi <Moezmahdy@gmail.com>
2018-11-12 23:55:25 +0300

    [Update] Added SSE Keep-Alive ApplicationEvent publishing in SseService

--- src/main/java/com/project/EMRChain/EmrChainApplication.java
+++ src/main/java/com/project/EMRChain/EmrChainApplication.java
@@ -1,13 +1,7 @@
 package com.project.EMRChain;
-import com.project.EMRChain.Entities.Auth.Role;
-import com.project.EMRChain.Models.RoleName;
-import com.project.EMRChain.Repositories.RoleRepository;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
-import org.springframework.transaction.annotation.Transactional;
-
 import javax.annotation.PostConstruct;
 import java.util.TimeZone;
 
---
+++ src/main/java/com/project/EMRChain/Services/SseService.java
@@ -0,0 +1,26 @@
+package com.project.EMRChain.Services;
+import com.project.EMRChain.Events.SseKeepAliveEvent;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationEventPublisher;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Service;
+
+@Service
+public class SseService
+{
+    private final ApplicationEventPublisher eventPublisher;
+
+    @Autowired
+    public SseService(ApplicationEventPublisher eventPublisher) {
+        this.eventPublisher = eventPublisher;
+    }
+
+
+    // Send an event every 4 minutes to keep the connection alive
+    @Scheduled(fixedRate = 240000)
+    public void SseKeepAlive()
+    {
+        SseKeepAliveEvent event = new SseKeepAliveEvent("0");
+        this.eventPublisher.publishEvent(event);
+    }
+}
