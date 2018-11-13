8f26156500fbcc6eb6bd2084c3283928b83718f2
MuizMahdi <Moezmahdy@gmail.com>
2018-11-13 22:58:52 +0300

    [Update] Added chain fetching ChainGet request process

--- src/main/java/com/project/EMRChain/Controllers/ChainController.java
+++ src/main/java/com/project/EMRChain/Controllers/ChainController.java
@@ -1,60 +1,85 @@
 package com.project.EMRChain.Controllers;
 import com.project.EMRChain.Core.Node;
 import com.project.EMRChain.Core.NodeCluster;
+import com.project.EMRChain.Events.GetChainFromProviderEvent;
 import com.project.EMRChain.Events.SseKeepAliveEvent;
+import com.project.EMRChain.Payload.Auth.ApiResponse;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.context.event.EventListener;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
 
 import java.io.IOException;
+import java.util.Map;
+import java.util.UUID;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
-import java.util.function.Consumer;
 
 
 @RestController
 @RequestMapping("/chain")
 public class ChainController
 {
+    private ApplicationEventPublisher eventPublisher;
+
+    @Autowired
+    public ChainController(ApplicationEventPublisher eventPublisher) {
+        this.eventPublisher = eventPublisher;
+    }
+
+
     private final Logger logger = LoggerFactory.getLogger(ChainController.class);
     private ExecutorService executorService = Executors.newCachedThreadPool();
 
     private NodeCluster chainProviders = new NodeCluster();
+    private NodeCluster chainConsumers = new NodeCluster();
 
 
     @GetMapping("/chainprovider/{nodeuuid}/{netuuid}")
-    public SseEmitter subscribeProvider(@PathVariable("nodeuuid") String nodeUUID, @PathVariable("netuuid") String networkUUID)
+    public SseEmitter subscribeProvider(@PathVariable("nodeuuid") String nodeUUID, @PathVariable("netuuid") String networkUUID) throws IOException
     {
         // Create an emitter for the subscribed client node
         SseEmitter emitter = new SseEmitter(2592000000L); // An extremely long timeout
 
-        // Create a node which has the emitter and the client's networkUUID
-        Node node = new Node(emitter, networkUUID);
+        if (!isValidUUID(nodeUUID) || !isValidUUID(networkUUID))
+        {
+            emitter.send("Invalid node or network UUID", MediaType.APPLICATION_JSON);
+        }
+        else
+        {
+            // Create a node which has the emitter and the client's networkUUID
+            Node node = new Node(emitter, networkUUID);
 
-        // Add the node to providers list
-        this.chainProviders.addNode(nodeUUID, node);
+            // Add the node to providers list
+            this.chainProviders.addNode(nodeUUID, node);
 
-        System.out.println("Node with netUUID: " + this.chainProviders.getNode(nodeUUID).getNetworkUUID() + " Was added to ChainProviders");
+            System.out.println("Node with netUUID: " + this.chainProviders.getNode(nodeUUID).getNetworkUUID() + " Was added to ChainProviders");
+        }
+
 
         // Remove the emitter on timeout/error/completion
-
         emitter.onTimeout(() -> {
             this.chainProviders.removeNode(nodeUUID);
             System.out.println("########## EMITTER TIMEOUT ##########");
         });
-
-        emitter.onError(error -> this.chainProviders.removeNode(nodeUUID));
 
         emitter.onCompletion(() -> {
             this.chainProviders.removeNode(nodeUUID);
             System.out.println("######### EMITTER COMPLETION #########");
         });
 
-        // Returns the GetChainFromProvider notification SSE
+        emitter.onError(error -> {
+            this.chainProviders.removeNode(nodeUUID);
+            System.out.println("########## EMITTER ERROR: " + error.getMessage() + " ##########");
+        });
+
+        // Returns the GetChainFromProviderEvent notification SSE
         return emitter;
     }
 
@@ -68,8 +93,11 @@ public class ChainController
     }
 
     @PostMapping("/chaingive")
-    public ResponseEntity chainGive()
+    public ResponseEntity chainGive(/* CHAIN REQUEST PAYLOAD RECEIVED HERE [Includes ConsumerUUID] */)
     {
+        // Publish a SendChainToGetter event, which causes a SSE to be sent through chainConsumer to the
+        // Node with the consumerUUID
+
         // Returns HttpStatus.OK on success
         return null;
     }
@@ -77,8 +105,49 @@ public class ChainController
     @GetMapping("/chainget/{consumeruuid}")
     public ResponseEntity ChainGet(@PathVariable("consumeruuid") String consumerUUID)
     {
+        boolean consumerExists = false;
+
+        // Go through the Chain Consumers and Check whether consumer
+        // exists on the chain consumers list or not
+        for (Map.Entry<String, Node> nodeEntry : chainConsumers.getCluster().entrySet())
+        {
+            if (nodeEntry.getKey().equals(consumerUUID)) {
+                consumerExists = true;
+            }
+        }
+
+        // If it consumer uuid is invalid or not in consumers list
+        if (!isValidUUID(consumerUUID) || !consumerExists)
+        {
+            return new ResponseEntity<>(
+                    new ApiResponse(false, "Invalid consumer UUID or doesn't exist"),
+                    HttpStatus.BAD_REQUEST
+            );
+        }
+
+        // Remove consumer from chain providers list
+        this.chainProviders.removeNode(consumerUUID);
+
+        try
+        {
+            // Get chain from a provider
+            GetChainFromProviderEvent chainFromProviderEvent = new GetChainFromProviderEvent(consumerUUID);
+            eventPublisher.publishEvent(chainFromProviderEvent);
+        }
+        catch (Exception Ex)
+        {
+            return new ResponseEntity<>(
+                new ApiResponse(false, "Invalid consumer UUID or doesn't exist"),
+                HttpStatus.BAD_REQUEST
+            );
+        }
+
+
         // Returns HttpStatus.OK on success
-        return null;
+        return new ResponseEntity<>(
+                new ApiResponse(true, "ChainGet request successfully sent"),
+                HttpStatus.OK
+        );
     }
 
     @GetMapping("/chainupdate")
@@ -116,6 +185,41 @@ public class ChainController
             }
 
         });
+    }
+
+    @EventListener
+    private void getChainFromProvider(GetChainFromProviderEvent event) throws IOException
+    {
+        // The passed on consumer UUID from chainGet() where the event is published
+        String consumerUUID = event.getConsumerUUID();
+
+        // Todo: Check if provider has a valid chain, if not then choose another, and so...
+
+        // Get a provider node from the chain providers
+        Map.Entry<String, Node> providerNode = this.chainProviders.getCluster().entrySet().iterator().next();
+
+        // Get the provider's emitter
+        SseEmitter providerEmitter = providerNode.getValue().getEmitter();
+
+
+        // Send a ChainRequest SSE through ChainProviders stream that contains the consumerUUID to the provider
+        providerEmitter.send(consumerUUID);
+    }
+
+    private boolean isValidUUID(String uuid)
+    {
+        if (uuid.isEmpty()) {
+            return false;
+        }
+
+        try {
+            UUID uuidFromString = UUID.fromString(uuid);
+        }
+        catch (IllegalArgumentException Ex) {
+            return false;
+        }
+
+        return true;
     }
 
 }
