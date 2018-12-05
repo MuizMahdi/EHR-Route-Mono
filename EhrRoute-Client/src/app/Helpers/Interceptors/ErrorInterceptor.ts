58e49c8a2283582073b4396df60ce10f64d59f2b
MuizMahdi <Moezmahdy@gmail.com>
2018-12-05 20:15:08 +0300

    [Update] Added Http Error Interceptor helper class

---
+++ EhrRoute-Client/src/app/Helpers/Interceptors/ErrorInterceptor.ts
@@ -0,0 +1,33 @@
+import { AuthService } from './../../Services/auth.service';
+import { Injectable } from "@angular/core";
+import { HttpInterceptor, HttpRequest, HttpHandler, HttpEvent } from "@angular/common/http";
+import { Observable, throwError } from 'rxjs';
+import { catchError } from 'rxjs/operators';
+
+
+@Injectable()
+export class ErrorInterceptor implements HttpInterceptor
+{
+   constructor(private authService:AuthService)
+   { }
+
+   intercept(request:HttpRequest<any>, next:HttpHandler): Observable<HttpEvent<any>>
+   {
+      return next.handle(request).pipe(
+
+         // Catch Http errors
+         catchError(error => {
+
+            if(error.status === 401) {
+               // Logout if http status code 401 response is returned
+               this.authService.logout;
+               location.reload(true);
+            }
+
+            const errorMessage = error.error.message || error.statusText;
+
+            return throwError(errorMessage);
+         })
+      )
+   }
+}
