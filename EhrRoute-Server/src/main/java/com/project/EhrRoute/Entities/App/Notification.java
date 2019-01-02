4878a3b9aaa129aaf93510c6bbace4602e9e51f7
MuizMahdi <Moezmahdy@gmail.com>
2019-01-02 20:43:55 +0300

    [Update] Added Notifications entity and NotificationType enum

---
+++ EhrRoute-Server/src/main/java/com/project/EhrRoute/Entities/App/Notification.java
@@ -0,0 +1,80 @@
+package com.project.EhrRoute.Entities.App;
+import com.project.EhrRoute.Audits.DateAudit;
+import com.project.EhrRoute.Entities.Auth.User;
+import com.project.EhrRoute.Models.NotificationType;
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+
+
+@Entity
+public class Notification extends DateAudit
+{
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotNull
+    @Column(columnDefinition = "tinyint(1) default 0") // Set to false as default value
+    private boolean isRead;
+
+    @JoinColumn(name = "sender_id", nullable = false)
+    private User sender;
+
+    @JoinColumn(name = "recipient_id", nullable = false)
+    private User recipient;
+
+    @NotNull
+    private NotificationType type;
+
+    @JoinColumn(name = "reference_id", nullable = false)
+    private Object reference;
+
+
+    public Notification() { }
+    public Notification(@NotNull boolean isRead, User sender, User recipient, @NotNull NotificationType type, Object reference) {
+        this.isRead = isRead;
+        this.sender = sender;
+        this.recipient = recipient;
+        this.type = type;
+        this.reference = reference;
+    }
+
+
+    public Long getId() {
+        return id;
+    }
+    public boolean isRead() {
+        return isRead;
+    }
+    public User getSender() {
+        return sender;
+    }
+    public User getRecipient() {
+        return recipient;
+    }
+    public NotificationType getType() {
+        return type;
+    }
+    public Object getReference() {
+        return reference;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+    public void setRead(boolean read) {
+        isRead = read;
+    }
+    public void setSender(User sender) {
+        this.sender = sender;
+    }
+    public void setType(NotificationType type) {
+        this.type = type;
+    }
+    public void setRecipient(User recipient) {
+        this.recipient = recipient;
+    }
+    public void setReference(Object reference) {
+        this.reference = reference;
+    }
+}
---
+++ EhrRoute-Server/src/main/java/com/project/EhrRoute/Models/NotificationType.java
@@ -0,0 +1,7 @@
+package com.project.EhrRoute.Models;
+
+public enum NotificationType
+{
+    CONSENT_REQUEST,
+    NETWORK_INVITATION
+}
