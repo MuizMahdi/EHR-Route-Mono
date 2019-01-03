939d07fe1ee25b694f70df9e7ed13fa05b6dfc32
MuizMahdi <Moezmahdy@gmail.com>
2019-01-03 20:34:39 +0300

    [Update] Added NetworkInvitationResponse payload fields

--- EhrRoute-Server/src/main/java/com/project/EhrRoute/Payload/App/NetworkInvitationResponse.java
+++ EhrRoute-Server/src/main/java/com/project/EhrRoute/Payload/App/NetworkInvitationResponse.java
@@ -1,7 +1,45 @@
 package com.project.EhrRoute.Payload.App;
 
-
 public class NetworkInvitationResponse
 {
+    private String recipientName;
+    private String networkName;
+    private String networkUUID;
+    private String token;
 
+
+    public NetworkInvitationResponse() { }
+    public NetworkInvitationResponse(String recipientName, String networkName, String networkUUID, String token) {
+        this.recipientName = recipientName;
+        this.networkName = networkName;
+        this.networkUUID = networkUUID;
+        this.token = token;
+    }
+
+
+    public String getToken() {
+        return token;
+    }
+    public String getNetworkName() {
+        return networkName;
+    }
+    public String getNetworkUUID() {
+        return networkUUID;
+    }
+    public String getRecipientName() {
+        return recipientName;
+    }
+
+    public void setToken(String token) {
+        this.token = token;
+    }
+    public void setNetworkName(String networkName) {
+        this.networkName = networkName;
+    }
+    public void setNetworkUUID(String networkUUID) {
+        this.networkUUID = networkUUID;
+    }
+    public void setRecipientName(String recipientName) {
+        this.recipientName = recipientName;
+    }
 }
