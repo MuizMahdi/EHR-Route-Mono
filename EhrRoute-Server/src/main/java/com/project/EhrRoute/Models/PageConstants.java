5b5fe49eb7b017867618b90d78abc8e140e3e3d1
MuizMahdi <Moezmahdy@gmail.com>
2019-01-04 19:12:05 +0300

    [Update] Added PageConstants Interface
    Contains Page related constants and constraints

---
+++ EhrRoute-Server/src/main/java/com/project/EhrRoute/Models/PageConstants.java
@@ -0,0 +1,13 @@
+package com.project.EhrRoute.Models;
+
+
+public interface PageConstants
+{
+    int MAX_PAGE_SIZE = 50;
+    int MAX_NOTIFICATIONS_PAGE_SIZE = 20;
+
+    String DEFAULT_PAGE_SIZE = "30";
+    String DEFAULT_NOTIFICATIONS_PAGE_SIZE = "10";
+
+    String DEFAULT_PAGE_NUMBER = "0";
+}
