37154fb5cc7a942d4019e1b9ea5aedab47818a0e
MuizMahdi <Moezmahdy@gmail.com>
2019-01-21 18:01:46 +0300

    [Update] Added search bar options

--- EhrRoute-Client/src/app/Components/MainLayout/nav-search/nav-search.component.ts
+++ EhrRoute-Client/src/app/Components/MainLayout/nav-search/nav-search.component.ts
@@ -1,15 +1,33 @@
 import { Component, OnInit } from '@angular/core';
 
+
 @Component({
   selector: 'app-nav-search',
   templateUrl: './nav-search.component.html',
   styleUrls: ['./nav-search.component.css']
 })
-export class NavSearchComponent implements OnInit {
 
-  constructor() { }
 
-  ngOnInit() {
-  }
+export class NavSearchComponent implements OnInit 
+{
+
+   // "Network" option is initially selected
+   selectedSearchOption:string = "Network"; 
+   searchInputValue:string = "";
+
+
+   constructor() 
+   { }
+
+
+   ngOnInit() 
+   { }
+
+   
+   onSearch()
+   {
+      console.log(this.selectedSearchOption);
+      console.log(this.searchInputValue);
+   }
 
 }
