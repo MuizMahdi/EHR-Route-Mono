056656954fc7d7c5e265854fa51b98cdef549bc5
MuizMahdi <Moezmahdy@gmail.com>
2019-02-17 15:32:40 +0300

    [Update] Added provider role check for side bar tabs visibility

--- EhrRoute-Client/src/app/Components/MainLayout/side-bar/side-bar.component.ts
+++ EhrRoute-Client/src/app/Components/MainLayout/side-bar/side-bar.component.ts
@@ -1,15 +1,25 @@
+import { AuthService } from './../../../Services/auth.service';
 import { Component, OnInit } from '@angular/core';
+
 
 @Component({
   selector: 'app-side-bar',
   templateUrl: './side-bar.component.html',
   styleUrls: ['./side-bar.component.css']
 })
-export class SideBarComponent implements OnInit {
 
-  constructor() { }
 
-  ngOnInit() {
-  }
+export class SideBarComponent implements OnInit 
+{
+   isUserProvider: boolean = false;
 
+
+   constructor(private authService:AuthService) { }
+
+
+   ngOnInit() {
+      if (this.authService.isUserProvider()) {
+         this.isUserProvider = true;
+      }
+   }
 }
