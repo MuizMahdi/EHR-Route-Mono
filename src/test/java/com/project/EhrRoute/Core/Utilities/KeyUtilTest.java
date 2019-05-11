86173606182163e09840374cf0515c9ee10e43bf
Muiz Mahdi <moezmahdy@gmail.com>
2019-05-12 02:18:14 +0300

    [Fix] Corrected sample RSA public key used in KeyUtil Test

--- src/test/java/com/project/EhrRoute/Core/Utilities/KeyUtilTest.java
+++ src/test/java/com/project/EhrRoute/Core/Utilities/KeyUtilTest.java
@@ -15,9 +15,15 @@ public class KeyUtilTest
     @Test
     public void getPublicKeyFromString() throws Exception
     {
-        String keyString = "MFYwEAYHKoZIzj0CAQYFK4EEAAoDQg" +
-        "AEj6KhGX6L9HOt2ClTMAfbYKSNPcEnX7GbmXfc1eRmLG5" +
-        "9z24P24BNbHQaKnROshGeDpHLuXl9Xd8ZSuHTYWQeQw==";
+        String keyString = "MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCg" +
+        "KCAQEAkOssFhIud24kdlMbjpYh3MJFGyFIQo2D5UisvLm/wJ5" +
+        "GJmGiI2R0IelKd11RxqpY+3E/fD6qWGamFdikwM9kms8hCr7A" +
+        "DXcl2W8c+qD95/u9OYlLlk+4vAmUUgABBQxp/nCRf8QIIYIZe" +
+        "Qn5j3lcn7MmPaa2NMJrhSSC2YnxKfnF5chO4DUhSnsXC+gMMR" +
+        "0hUfnBR++3Wq4ziqxALX8u2r7WRI/Hyr2Vm/kQtpgfbMCFNyg" +
+        "Iij0yjaz73A8qgO2R9fvf++Z8DULYbEJWViBmBcT5EE8n4EIl" +
+        "t7FyZ43S3lRso8ItYyc508+2wYBhWw9l+BvTGsYtNxcWi6z5f5" +
+        "Fm+wIDAQAB";
 
         keyUtil.getPublicKeyFromString(keyString);
 
